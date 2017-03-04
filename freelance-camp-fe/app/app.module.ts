b0b7b024582f47df4e119678c0d5b77d5fa0f3ea
Vincent LI <daybreaklee@utexas.edu>
2017-03-03 21:38:19 -0600

    modulize home

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomepageComponent } from './homepage.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ HomepageComponent ],
-    bootstrap: [ HomepageComponent ]
+    declarations: [
+        AppComponent,
+        HomepageComponent ],
+    bootstrap: [ AppComponent ]
 })
 
 export class AppModule {
